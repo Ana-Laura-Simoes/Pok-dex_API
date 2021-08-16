c6bf5f39e80e78a4a70b0e6265948ccbb2b2d4f6
Guga <contato.gustavobarbosa@outlook.com>
2021-08-16 15:46:24 -0300

    Preparando revisão do código da Ana

--- tests/utils/database.ts
+++
@@ -1,8 +0,0 @@
-import {getManager } from "typeorm";
-
-export async function clearDatabase () {
-  await getManager().query(`TRUNCATE "users" RESTART IDENTITY CASCADE`);
-  await getManager().query(`TRUNCATE "sessions" RESTART IDENTITY CASCADE`);
-  await getManager().query(`TRUNCATE "pokemons" RESTART IDENTITY CASCADE`);
-  await getManager().query(`TRUNCATE "userPokemons" RESTART IDENTITY CASCADE`);
-}
